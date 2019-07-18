baf88cfb63501d807935b99931139c19aff8e777
Kelly davis <kellyadavis7@gmail.com>
2019-07-18 14:24:37 -0400

    Small display changes.

--- src/components/about.js
+++ src/components/about.js
@@ -7,7 +7,9 @@ function About() {
 
       <p>kellyadavis7@gmail.com</p>
       <a href="https://github.com/kad1001">Github</a>
+      <br></br>
       <a href="https://www.linkedin.com/in/kelly-davis-573316a9/">LinkedIn</a>
+      <br></br>
       <a href="https://docs.google.com/document/d/1uRTxTkqtdoU0VhYGafJpmO7IEYtSeUiJdZvduwW8guk/edit?usp=sharing">Resume</a>
     </div>
   );
